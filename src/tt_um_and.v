ecd216fc5a7ca5545e94d72a5119243cd238c401
Zach-Stealth-Wilson <123966207+Zach-Stealth-Wilson@users.noreply.github.com>
2024-05-17 16:42:39 -0500

    Update tt_um_and.v

--- src/tt_um_and.v
+++ src/tt_um_and.v
@@ -42,4 +42,10 @@ module tt_um_and(
   	//assign A = clk_div2;
   assign Y = clk_div2 & clk_div8;
 
+	  // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+
 endmodule
