168240eced1a15dbaac28f1db4d22614310b9e48
Zach-Stealth-Wilson <123966207+Zach-Stealth-Wilson@users.noreply.github.com>
2024-05-17 16:41:34 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -14,6 +14,11 @@ module tt_um_example (
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
+      output Y,
+  output reg clk_div2,
+  output reg clk_div4,
+  output reg clk_div8,
+  output reg clk_div16
 );
 
   // All output pins must be assigned. If not used, assign to 0.
