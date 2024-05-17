b51489abc5cafd8ddceed956f57ecaab3897f7ba
Zach-Stealth-Wilson <123966207+Zach-Stealth-Wilson@users.noreply.github.com>
2024-05-17 16:35:54 -0500

    Update tt_um_and.v

--- src/tt_um_and.v
+++ src/tt_um_and.v
@@ -7,6 +7,11 @@ flip-flop method.
 module tt_um_and(
   input clk,
   input reset,
+	input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
   input ena,
   input  wire       rst_n,     // reset_n - low to reset
  // input wire A,
