6556e22b1d33b212eb40d2f3c014020c9510084f
Zach-Stealth-Wilson <123966207+Zach-Stealth-Wilson@users.noreply.github.com>
2024-05-17 15:53:34 -0500

    Update tt_um_and.v

--- src/tt_um_and.v
+++ src/tt_um_and.v
@@ -1,7 +1,9 @@
-module tt_um_and(clk,A,B,Y);
-  input clk;
+module tt_um_and(A,B,Y,ena,clk,rst_n);
   input [7:0] A;
   input [7:0] B;
   output [7:0] Y;
+  input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+  input  wire       clk,      // clock
+  input  wire       rst_n     // reset_n - low to reset
   assign Y = A & B;
 endmodule
