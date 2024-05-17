e0e4a653625529cfc037599ffad526aba933c42f
Zach-Stealth-Wilson <123966207+Zach-Stealth-Wilson@users.noreply.github.com>
2024-05-17 16:16:20 -0500

    Update tt_um_and.v

--- src/tt_um_and.v
+++ src/tt_um_and.v
@@ -1,9 +1,38 @@
-module tt_um_and(A,B,Y,ena,clk,rst_n);
-  input [7:0] A;
-  input [7:0] B;
-  output [7:0] Y;
-  input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-  input  wire       clk,      // clock
-  input  wire       rst_n     // reset_n - low to reset
-  assign Y = A & B;
+
+/*
+A clock divider in Verilog, using the cascading
+flip-flop method.
+*/
+
+module tt_um_and(
+  input clk,
+  input reset,
+ // input wire A,
+ // input wire B,
+  output Y,
+  output reg clk_div2,
+  output reg clk_div4,
+  output reg clk_div8,
+  output reg clk_div16
+);
+
+  // simple ripple clock divider
+  	
+ 
+ 
+  always @(posedge clk)
+    clk_div2 <= reset ? 0 : ~clk_div2;
+	
+  always @(posedge clk_div2)
+    clk_div4 <= ~clk_div4;
+  	
+
+  always @(posedge clk_div4)
+    clk_div8 <= ~clk_div8;
+
+  always @(posedge clk_div8)
+    clk_div16 <= ~clk_div16;
+  	//assign A = clk_div2;
+  assign Y = clk_div2 & clk_div8;
+
 endmodule
