5952fddeb06ad929a14aaaedc32333ef385aeab6
Zach-Stealth-Wilson <123966207+Zach-Stealth-Wilson@users.noreply.github.com>
2024-05-17 16:42:16 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -35,7 +35,7 @@ module tt_um_example (
   	//assign A = clk_div2;
   assign Y = clk_div2 & clk_div8;
 
-endmodule
+
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
